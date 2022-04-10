31d66f9bb06ea483369f9a3ab90026127fa51c1d
senayberhe <tsionberhe@gmail.com>
2022-04-09 23:36:14 -0700

    added files

--- stores/serializers.py
+++ stores/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Pizzeria
+from rest_framework.reverse import reverse
 
 class PizzeriaListSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = Pizzeria
         fields = [
@@ -9,11 +11,16 @@ class PizzeriaListSerializer(serializers.ModelSerializer):
             'pizzeria_name',
             'city',
             'zip_code',
+            'absolute_url'
         ]
         
+    def get_absolute_url(self, obj):
+        return reverse('pizzeria_detail', args=(obj.pk,))
         
 
 class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    update = serializers.SerializerMethodField()
+    delete = serializers.SerializerMethodField()
     class Meta:
         model = Pizzeria
         fields = [ 
@@ -28,5 +35,14 @@ class PizzeriaDetailSerializer(serializers.ModelSerializer):
             'description',
             'logo_image',
             'email',
-            'active',          
+            'active',  
+            'update',   
+            'delete',     
         ]
+        
+    def get_update(self, obj):
+        return reverse('pizzeria_update', args=(obj.pk,))
+    
+    
+    def get_delete(self, obj):
+        return reverse('pizzeria_delete', args=(obj.pk,))
